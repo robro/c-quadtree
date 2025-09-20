d475f1ba3f1fe40aebfbe7ce0bde2ebb0a605735
robro <pibborando@gmail.com>
2025-09-20 00:17:33 -0700

    Make adding shapes to quadtree more generic

--- quadtree.c
+++ quadtree.c
@@ -55,17 +55,19 @@ void quadtree_free(QuadTree *qtree) {
 	free(qtree);
 }
 
-bool quadtree_node_add_point(QuadTree *qtree, int index, Vec2 *point) {
+typedef bool RectIntersectsShapeFunc(Rect *, void *);
+
+bool quadtree_node_add_shape(QuadTree *qtree, int index, void *shape, RectIntersectsShapeFunc func) {
 	QuadTreeNode *node = &qtree->nodes[index];
 
-	if (!rect_intersects_point(&node->boundary, point)) {
+	if (!func(&node->boundary, shape)) {
 		return false;
 	}
 	if (node->entity_count < QT_NODE_CAPACITY && node->child_indices[0] < 0) {
 		// we have room for more entities and no children yet
 		// assume if any child indices are invalid they all are
 		// so just add the entity here and increment entity count
-		node->entities[node->entity_count++] = point;
+		node->entities[node->entity_count++] = shape;
 		return true;
 	}
 	if (node->child_indices[0] < 0 && qtree->size > qtree->capacity - 4) {
@@ -111,151 +113,39 @@ bool quadtree_node_add_point(QuadTree *qtree, int index, Vec2 *point) {
 	// add new entity to whichever child will accept it
 	// it should always be accepted unless something weird has happened
 	for (int i = 0; i < 4; ++i) {
-		if (quadtree_node_add_point(qtree, node->child_indices[i], point)) return true;
+		if (quadtree_node_add_shape(qtree, node->child_indices[i], shape, func)) return true;
 	}
 	printf("ERROR: Reached unreachable code!\n");
 	return false;
 }
 
-bool quadtree_node_add_rect(QuadTree *qtree, int index, Rect *rect) {
-	QuadTreeNode *node = &qtree->nodes[index];
-
-	if (!rect_intersects_rect(&node->boundary, rect)) {
-		return false;
-	}
-	if (node->entity_count < QT_NODE_CAPACITY && node->child_indices[0] < 0) {
-		// we have room for more entities and no children yet
-		// assume if any child indices are invalid they all are
-		// so just add the entity here and increment entity count
-		node->entities[node->entity_count++] = rect;
-		return true;
-	}
-	if (node->child_indices[0] < 0 && qtree->size > qtree->capacity - 4) {
-		// realloc the nodes to fit more entities (double capacity)
-		QuadTreeNode *new_nodes = realloc(
-			qtree->nodes, sizeof(*new_nodes) * qtree->capacity * 2
-		);
-		if (new_nodes == NULL) {
-			printf("ERROR: Failed to allocate new memory! Can't add point!\n");
-			return false;
-		}
-		qtree->nodes = new_nodes;
-		qtree->capacity *= 2;
-		node = &qtree->nodes[index];
-		printf("QuadTree node capacity doubled to: %d nodes\n", qtree->capacity);
-	}
-	if (node->child_indices[0] < 0) {
-		// we don't have room for more entities and need to subdivide
-		for (int i = 0; i < 4; ++i) {
-			node->child_indices[i] = qtree->size + i;
-		}
-
-		Vec2 boundary_center = rect_get_center(&node->boundary);
-
-		quadtree_node_init(&qtree->nodes[node->child_indices[0]], &(Rect){
-			.min = node->boundary.min,
-			.max = boundary_center,
-		});
-		quadtree_node_init(&qtree->nodes[node->child_indices[1]], &(Rect){
-			.min = {.x = boundary_center.x, .y = node->boundary.min.y},
-			.max = {.x = node->boundary.max.x, .y = boundary_center.y}
-		});
-		quadtree_node_init(&qtree->nodes[node->child_indices[2]], &(Rect){
-			.min = {.x = node->boundary.min.x, .y = boundary_center.y},
-			.max = {.x = boundary_center.x, .y = node->boundary.max.y}
-		});
-		quadtree_node_init(&qtree->nodes[node->child_indices[3]], &(Rect){
-			.min = boundary_center,
-			.max = node->boundary.max,
-		});
-		qtree->size += 4;
-	}
-	// add new entity to all children that will accept it (it can be in multiple)
-	// it should always be accepted unless something weird has happened
-	for (int i = 0; i < 4; ++i) {
-		if (quadtree_node_add_rect(qtree, node->child_indices[i], rect)) return true;
-	}
-	printf("ERROR: Reached unreachable code!\n");
-	return false;
+bool _rect_intersects_point(Rect *rect, void *point) {
+	return rect_intersects_point(rect, point);
 }
 
-bool quadtree_node_add_circle(QuadTree *qtree, int index, Circle *circle) {
-	QuadTreeNode *node = &qtree->nodes[index];
+bool _rect_intersects_rect(Rect *rect1, void *rect2) {
+	return rect_intersects_rect(rect1, rect2);
+}
 
-	if (!rect_intersects_circle(&node->boundary, circle)) {
-		return false;
-	}
-	if (node->entity_count < QT_NODE_CAPACITY && node->child_indices[0] < 0) {
-		// we have room for more entities and no children yet
-		// assume if any child indices are invalid they all are
-		// so just add the entity here and increment entity count
-		node->entities[node->entity_count++] = circle;
-		return true;
-	}
-	if (node->child_indices[0] < 0 && qtree->size > qtree->capacity - 4) {
-		// realloc the nodes to fit more entities (double capacity)
-		QuadTreeNode *new_nodes = realloc(
-			qtree->nodes, sizeof(*new_nodes) * qtree->capacity * 2
-		);
-		if (new_nodes == NULL) {
-			printf("ERROR: Failed to allocate new memory! Can't add point!\n");
-			return false;
-		}
-		qtree->nodes = new_nodes;
-		qtree->capacity *= 2;
-		node = &qtree->nodes[index];
-		printf("QuadTree node capacity doubled to: %d nodes\n", qtree->capacity);
-	}
-	if (node->child_indices[0] < 0) {
-		// we don't have room for more entities and need to subdivide
-		for (int i = 0; i < 4; ++i) {
-			node->child_indices[i] = qtree->size + i;
-		}
-
-		Vec2 boundary_center = rect_get_center(&node->boundary);
-
-		quadtree_node_init(&qtree->nodes[node->child_indices[0]], &(Rect){
-			.min = node->boundary.min,
-			.max = boundary_center,
-		});
-		quadtree_node_init(&qtree->nodes[node->child_indices[1]], &(Rect){
-			.min = {.x = boundary_center.x, .y = node->boundary.min.y},
-			.max = {.x = node->boundary.max.x, .y = boundary_center.y}
-		});
-		quadtree_node_init(&qtree->nodes[node->child_indices[2]], &(Rect){
-			.min = {.x = node->boundary.min.x, .y = boundary_center.y},
-			.max = {.x = boundary_center.x, .y = node->boundary.max.y}
-		});
-		quadtree_node_init(&qtree->nodes[node->child_indices[3]], &(Rect){
-			.min = boundary_center,
-			.max = node->boundary.max,
-		});
-		qtree->size += 4;
-	}
-	// add new entity to all children that will accept it (it can be in multiple)
-	// it should always be accepted unless something weird has happened
-	for (int i = 0; i < 4; ++i) {
-		if (quadtree_node_add_circle(qtree, node->child_indices[i], circle)) return true;
-	}
-	printf("ERROR: Reached unreachable code!\n");
-	return false;
+bool _rect_intersects_circle(Rect *rect, void *circle) {
+	return rect_intersects_circle(rect, circle);
 }
 
 void quadtree_add_points(QuadTree *qtree, Vec2 *points, int point_count) {
 	for (int i = 0; i < point_count; ++i) {
-		quadtree_node_add_point(qtree, 0, &points[i]);
+		quadtree_node_add_shape(qtree, 0, &points[i], _rect_intersects_point);
 	}
 }
 
 void quadtree_add_rects(QuadTree *qtree, Rect *rects, int rect_count) {
 	for (int i = 0; i < rect_count; ++i) {
-		quadtree_node_add_rect(qtree, 0, &rects[i]);
+		quadtree_node_add_shape(qtree, 0, &rects[i], _rect_intersects_rect);
 	}
 }
 
 void quadtree_add_circles(QuadTree *qtree, Circle *circles, int circle_count) {
 	for (int i = 0; i < circle_count; ++i) {
-		quadtree_node_add_circle(qtree, 0, &circles[i]);
+		quadtree_node_add_shape(qtree, 0, &circles[i], _rect_intersects_circle);
 	}
 }
 
